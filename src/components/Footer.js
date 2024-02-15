3630c6dc3531c6bc98c384d59c650f48c641131d
Binu Peter <binupeterp07@gmail.com>
2024-02-15 13:43:09 +0000

    Added fb url to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,19 @@
 import React from "react";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import FacebookIcon from "@mui/icons-material/Facebook";
-import "../styles/Footer.css"
+import "../styles/Footer.css";
+import { Link } from "react-scroll";
 
 const Footer = () => {
   return (
     <div className="footer">
       <div className="socialMedia">
-        <InstagramIcon />
-        <FacebookIcon />
+        <Link>
+          <InstagramIcon />
+        </Link>
+        <Link to="https://www.facebook.com/profile.php?id=61556260496193">
+          <FacebookIcon />
+        </Link>
       </div>
       <p> &copy; 2024 Hazolt</p>
     </div>
