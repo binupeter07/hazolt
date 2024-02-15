a262d7a3e0a14d9c31f9ff57a2c1c4952494b7e5
Binu Peter <binupeterp07@gmail.com>
2024-02-15 13:52:16 +0000

    Make more responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,20 @@
-import React from "react";
-import InstagramIcon from "@mui/icons-material/Instagram";
-import FacebookIcon from "@mui/icons-material/Facebook";
-import "../styles/Footer.css";
-import { Link } from "react-scroll";
+import React from 'react';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import '../styles/Footer.css';
+// No need to import Link from 'react-scroll' for the Facebook link
 
 const Footer = () => {
   return (
     <div className="footer">
       <div className="socialMedia">
-        <Link>
+        {/* Keep Instagram link as is or modify as needed for in-page navigation or external link */}
+        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
           <InstagramIcon />
-        </Link>
-        <Link to="https://www.facebook.com/profile.php?id=61556260496193">
+        </a>
+        <a href="https://www.facebook.com/profile.php?id=61556260496193" target="_blank" rel="noopener noreferrer">
           <FacebookIcon />
-        </Link>
+        </a>
       </div>
       <p> &copy; 2024 Hazolt</p>
     </div>
